a2d47e461c7ba419867a75226495a4b3f193e6fc
joya <joyaestephan@outlook.com>
2023-06-20 22:14:32 +0300

    new password change

---
+++ connect.php
@@ -0,0 +1,17 @@
+<?php 
+$conn = new mysqli('localhost' , 'root' , '' , 'bookmycare');
+// if (!$conn){
+//     die(mysqli_error($conn)); 
+// }
+if (!$conn){
+    die(mysqli_error($conn));
+}
+
+
+?>
+
+
+
+
+
+
