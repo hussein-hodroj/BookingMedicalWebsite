462d345c894ed6e6b09498ba4688efd6ca63eece
hussein-hodroj <132451968+hussein-hodroj@users.noreply.github.com>
2023-06-25 15:52:21 +0300

    rejection email

--- admin/send_rejection_email.php
+++ admin/send_rejection_email.php
@@ -1,29 +1,48 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $rejectionReason = $_POST['rejectionReason'];
-    $doctorId = $_POST['doctorId'];
-
-    // Retrieve the doctor's email from the database based on the doctor ID
-    // Replace this with your actual code to retrieve the email
-    $email = "doctor@example.com"; // Example email, replace with your implementation
-
-    // Compose the email message
-    $subject = "Application Rejection";
-    $message = "Dear Doctor,\n\n";
-    $message .= "Your application has been rejected for the following reason:\n";
-    $message .= $rejectionReason;
-
-    // Send the email
-    $headers = "From: Your Name <your-email@example.com>"; // Replace with your name and email address
-    if (mail($email, $subject, $message, $headers)) {
-        // Email sent successfully
-        echo "Email sent successfully";
+require '../PHPMailer/src/Exception.php';
+require '../PHPMailer/src/PHPMailer.php';
+require '../PHPMailer/src/SMTP.php';
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+if (isset($_POST['submit'])) {
+    $email = filter_input(INPUT_POST, 'emailInput', FILTER_SANITIZE_EMAIL);
+    if ($email !== false && filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $rejectionReason = $_POST['rejectionReason'];
+        sendRejectionEmail($email, $rejectionReason);
+        
+        deleteUser($_POST['deleteId']);
+        header("Location: ../doctorconf.php?msg=Email has been sent and user has been deleted");
+        exit();
     } else {
-        // Failed to send email
-        echo "Failed to send email";
+        header("Location: ../doctorconf.php?msgemail=Invalid email address");
+        exit();
     }
-} else {
-    // Invalid request
-    echo "Invalid request";
+}
+
+function sendRejectionEmail($email, $rejectionReason) {
+    $mail = new PHPMailer(true);
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'medmedresetpass@gmail.com';
+    $mail->Password = 'bvuilvevngoglngx';
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port = 465;
+    $mail->setFrom('medmedresetpass@gmail.com');
+    $mail->addAddress($email);
+    $mail->isHTML(true);
+    $mail->Subject = 'Application Rejection';
+    $mail->Body = $rejectionReason;
+    $mail->send();
+}
+
+function deleteUser($id) {
+    include '../connect.php';
+    $stmt = $conn->prepare('DELETE FROM user WHERE id = ?');
+    $stmt->bind_param('s', $id);
+    $stmt->execute();
+    $stmt->close();
+    $conn->close();
 }
 ?>
